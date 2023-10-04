61497ea6654315eacd89307051380d53efe3aab5
Lucia <adri.alvrzt@gmail.com>
2023-10-04 18:36:39 +0200

    ap05 finish

--- components/Ap05.vue
+++ components/Ap05.vue
@@ -51,12 +51,11 @@
 							</span>
 						</a>
 					</div>
-					<img src="/images/AP01-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP01-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Ara que ja estem a la pàgina d'instal·lació donar-li al botó de descarregar (foto anterior) , això començarà la descarrega del
-						ISO.
+						Ara que ja estem a la pàgina d'instal·lació donar-li al botó de descarregar, això començarà la descarrega del ISO.
 					</h1>
-					<img src="/images/AP02-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP02-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
 						Ara que ja tenim la ISO instal·lada, ens dirigim al VirtualBox i donem a "New".
 					</h1>
@@ -64,76 +63,88 @@
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Se'ns obrirà aquesta pestanya:"</h1>
 					<img src="/images/AP05-2sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Posem de nom a la màquina "Android" i seleccionem l'iso que acabem de descarregar. I donem a "Next"
+						Posem de nom a la màquina "Android" i seleccionem l'iso que acabem de descarregar. A la versió posem Ubuntu 64 Bits, és
+						important per l'instal·lació. I donem a "Next"
 					</h1>
-					<img src="/images/AP03-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP03-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
 						Ara ens preguntarà quanta RAM i quants processadors li posarem a aquesta màquina, segons diu la pàgina de Android, amb 4GB de
 						RAM i 2 processador ja vas bé. Una vegada acabat de possar tot el que volem li donem a "Next"
 					</h1>
-					<img src="/images/AP04-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP04-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Ara ens pregunta quant d'espai donarem a la màquina, a la pàgin de Android posa uns 40GB. Un cop posats li donem a "Next"
+						Ara ens pregunta quant d'espai donarem a la màquina, a la pàgin de Android posa uns 10GB. Un cop posats li donem a "Next"
 					</h1>
-					<img src="/images/AP05-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP05-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
 						Ara ens surt una pestanya de confirmació on surt com es crearà la màquina virtual, si estem d'acord donem a "Finish".
 					</h1>
-					<img src="/images/AP06-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP06-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
 						I ja tindriem la nostra màquina virtual afegida al VirtualBox, però falta instal·lar-la, per això encendrem la màquina
 						donant-li dos clics.
 					</h1>
-					<img src="/images/AP08-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP07-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
 						<!-- Ara ens pregunta quin idioma volem per a la instal·lació i també serà l'idioma de l'OS, seleccionem el que més ens agradi, en
 						el meu cas posaré espanyol i li donem a Instalar Android. -->
-						Li donarem a "Start Android-Workstation-Live 38" per iniciar Android.
+						Li donarem a "Installation - Install Android-x86 to hard disk".
 					</h1>
-					<img src="/images/AP09-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP08-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Ara seleccionarem "Create/Modify partitions".</h1>
+					<img src="/images/AP09-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Li donarem que no.</h1>
+					<img src="/images/AP10-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Li donarem a "Install Android" per començar l'instal·lació de Android.
+						Ara necessitem donar-li al botó de "New", una vegada donat, ens preguntarà si volem que la partició sigui lògica o primària,
+						li donem a primària. Posem l'espai del nostre disc a MB (o li donem a enter) I així ens quedaria el resultat:
 					</h1>
-					<img src="/images/AP10-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP11-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Ara donem a "Bootable"</h1>
+					<img src="/images/AP12-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">I donem a "Write" i posem "Yes" per confirmar.</h1>
+					<img src="/images/AP13-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Ara ens pregunta quin idioma volem per a la instal·lació i també serà l'idioma de l'OS, seleccionem el que més ens agradi, en
-						el meu cas posaré espanyol i li donem a Continuar.
+						Ara seleccionem la partició que acabem de crear i donem a "ok".
 					</h1>
-					<img src="/images/AP11-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP14-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Ara ens diu quina serà la nostra distribució del teclat i quina serà la zona horària del temps. També ens posa que seleccionem
-						el nostre disc on instal·larem Android, per això cliquem i seleccionem el disc que vulguem, quan el seleccions li dónes a fet
-						i per últim li donem a Començar la instal·lació.
+						Ara seleccionem ext4 que és el tipus de fitxers de Linux i Android és un tipus de Linux. I donem a "Ok".
 					</h1>
-					<img src="/images/AP12-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
-					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Esperem a que s'instali</h1>
-					<img src="/images/AP13-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP15-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Ara li donem a "Yes" per confirmar.</h1>
+					<img src="/images/AP16-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Li donem a "yes" una altre vegada y comenzará a instalarse.</h1>
+					<img src="/images/AP17-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
 					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
-						Un cop finalitzada l'instal·lació li donem a finalitzar l'instalació i es reiniciarà.
+						Un cop acaba la instal·lació ens diu que volem fer, donem a "Run Android-x86" i donem a "ok".
 					</h1>
-					<img src="/images/AP14-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
-					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">I ja tindriem Android instal·lat i llest per utilitzar.</h1>
-					<img src="/images/AP15-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
-					<hr class="mx-auto my-32 h-1 w-48 rounded border-0 dark:bg-gray-500" />
-				</div>
-				<div id="guest edition">
-					<div class="w-full">
-						<h1 class="mt-6 bg-gradient-to-r from-sky-300 to-blue-500 bg-clip-text text-lg font-bold text-transparent lg:text-2xl">
-							Instalació del Guest Addition
-							<hr class="my-2 h-1 w-full rounded border-0 bg-gradient-to-br from-sky-300 to-blue-500" />
-						</h1>
-					</div>
-					<h1 class="text-base font-semibold text-white lg:text-xl">
-						Per instal·lar el Guest Addition hauríem d'anar a "Devices" i donar-li a l'opció de "Insert Guest Addition CD Image..."
+					<img src="/images/AP18-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
+						Ara ens pregunta quin idioma volem, seleccionem el que més ens agradi, en el meu cas posaré espanyol i li donem a començar.
 					</h1>
-					<img src="/images/AP16-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
-					<h1 class="text-base font-semibold text-white lg:text-xl">Esperem a que s'instali.</h1>
-					<img src="/images/AP17-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
-					<h1 class="text-base font-semibold text-white lg:text-xl">
-						Un cop acabada la descàrrega ja tindríem el nostre Android instal·lat amb Guest Addition i llest per fer el que vulguem amb
-						ell.
+					<img src="/images/AP19-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">Ahora ens pregunta si volem conectar wifi, li donem a saltar.</h1>
+					<img src="/images/AP20-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
+						Ahora ens pregunta si la data i l'hora estàn bé, li donem a seguent.
 					</h1>
-					<img src="/images/AP19-4sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<img src="/images/AP21-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
+						Ens pregunta si volem donar permisos per a la ubicació i que accedeixin a les nostres dades, jo li he donat que no, però tu
+						pots posar el que vulguis. Donem a aceptar.
+					</h1>
+					<img src="/images/AP22-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
+						Ara ens demana una contrasenya per accedir al sistema, si vols posar una contrasenya selecciona la que creguis convenient, si
+						no, com és el meu cas, dónes ara no.
+					</h1>
+					<img src="/images/AP23-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+					<h1 class="mt-2 text-base font-semibold text-white lg:text-xl">
+						I ja tindriem el nostre sistema operatiu de Android funcionant perfectament! (No té Guest Addition)
+					</h1>
+					<img src="/images/AP24-5sistema.png" alt="foto" class="mx-auto mt-4 w-[200px] rounded-md md:w-[600px]" />
+
 					<hr class="mx-auto my-32 h-1 w-48 rounded border-0 dark:bg-gray-500" />
 				</div>
 			</div>
